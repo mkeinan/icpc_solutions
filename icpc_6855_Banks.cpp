36f0a44c4ba82f7bbfc1b5d49d139a6684e7c83d
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2019-10-18 20:47:18 +0300

    Version for icpc 6855 Banks that seems to be correct on example input found,
    but it gets Wrong Answer on ICPC judge.

--- icpc_6855_Banks.cpp
+++ icpc_6855_Banks.cpp
@@ -31,7 +31,11 @@ bool is_final_state(vector<int> state, int n) {
 	return all_pos;
 }
 
-int bfs(int n) {
+
+
+
+
+int bfs4(int n) {
 	while (!qu.empty()) {
 		pair<vector<int>, int> cur_pair = qu.front();
 		qu.pop();
@@ -45,6 +49,154 @@ int bfs(int n) {
 //			cout << state[i] << " ";
 //		}
 //		cout << "     depth " << depth << endl;
+
+		if (is_final_state(state, n)) {
+			return depth;
+		}
+
+		int lowest_abs_sum = 2147483647;
+//		long int lowest_abs_sum = (long int)9223372036854775807;
+		vector<int> chosen_state;
+		for (int i = 0; i < n; i++) {
+			if (state[i] < 0) {
+				vector<int> new_state = state;
+				new_state[i] = abs(state[i]);
+				new_state[(i - 1 + n) % n] -= abs(state[i]);
+				new_state[(i + 1 + n) % n] -= abs(state[i]);
+
+//				qu.push(make_pair(new_state, depth + 1));
+
+				int abs_sum = 0;
+				for (int k = 0; k < n; k++) {
+					abs_sum += abs(new_state[k]);
+				}
+				if (abs_sum < lowest_abs_sum){
+					lowest_abs_sum = abs_sum;
+					chosen_state = new_state;
+				}
+			}
+		}
+		qu.push(make_pair(chosen_state, depth + 1));
+//		processed.insert(state);
+	}
+	return -1;  // Error
+}
+
+
+/*
+int limit = 2147483647;
+
+int bfs3(int n) {
+	while (!qu.empty()) {
+		pair<vector<int>, int> cur_pair = qu.front();
+		qu.pop();
+		vector<int> state = cur_pair.first;
+		if (processed.find(state) != processed.end()){
+			continue;
+		}
+		int depth = cur_pair.second;
+
+		for (int i = 0; i < n; i++) {
+			cout << state[i] << " ";
+		}
+		cout << "     depth " << depth << endl;
+
+		if (is_final_state(state, n)) {
+			return depth;
+		}
+
+		int abs_sum = 0;
+		for (int i = 0; i < n; i++) {
+			abs_sum += abs(state[i]);
+		}
+		if (abs_sum > limit){
+			cout << "     BACKTRACK CUT! " << depth << endl;
+			continue;
+		} else {
+			limit = abs_sum;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (state[i] < 0) {
+				vector<int> new_state = state;
+				new_state[i] = abs(state[i]);
+				new_state[(i - 1 + n) % n] -= abs(state[i]);
+				new_state[(i + 1 + n) % n] -= abs(state[i]);
+
+				qu.push(make_pair(new_state, depth + 1));
+			}
+		}
+		processed.insert(state);
+	}
+	return -1;  // Error
+}
+
+
+int dir = 0;
+
+int bfs2(int n) {
+	while (!qu.empty()) {
+		pair<vector<int>, int> cur_pair = qu.front();
+		qu.pop();
+		vector<int> state = cur_pair.first;
+		if (processed.find(state) != processed.end()){
+			continue;
+		}
+		int depth = cur_pair.second;
+
+		for (int i = 0; i < n; i++) {
+			cout << state[i] << " ";
+		}
+		cout << "     depth " << depth << endl;
+
+		if (is_final_state(state, n)) {
+			return depth;
+		}
+
+		// just find the smallest and turn it...
+
+		int min = state[0];
+		int idx_min = 0;
+		if (dir == 0) {
+			for (int i = 0; i < n; i++) {
+				if (state[i] < min) {
+					min = state[i];
+					idx_min = i;
+				}
+			}
+		} else {
+			for (int i = n-1; i >= 0; i--) {
+				if (state[i] < min) {
+					min = state[i];
+					idx_min = i;
+				}
+			}
+		}
+		vector<int> new_state = state;
+		new_state[idx_min] = abs(state[idx_min]);
+		new_state[(idx_min - 1 + n) % n] -= abs(state[idx_min]);
+		new_state[(idx_min + 1 + n) % n] -= abs(state[idx_min]);
+
+		qu.push(make_pair(new_state, depth + 1));
+		processed.insert(state);
+	}
+	return -1;  // Error
+}
+
+int bfs(int n) {
+	while (!qu.empty()) {
+		pair<vector<int>, int> cur_pair = qu.front();
+		qu.pop();
+		vector<int> state = cur_pair.first;
+		if (processed.find(state) != processed.end()){
+			continue;
+		}
+		int depth = cur_pair.second;
+
+		for (int i = 0; i < n; i++) {
+			cout << state[i] << " ";
+		}
+		cout << "     depth " << depth << endl;
 
 		if (is_final_state(state, n)) {
 			return depth;
@@ -64,17 +216,18 @@ int bfs(int n) {
 	}
 	return -1;  // Error
 }
-
+*/
 
 void solve(int n) {
 	vector<int> banks(n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &banks[i]);
 	}
-	int sum_of_elems = accumulate(banks.begin(), banks.end(), 0);
-	assert(sum_of_elems > 0);
+//	int sum_of_elems = accumulate(banks.begin(), banks.end(), 0);
+//	assert(sum_of_elems > 0);
 	qu.push(make_pair(banks, 0));
-	int ans = bfs(n);
+//	int ans = bfs(n);
+	int ans = bfs4(n);
 	printf("%d\n", ans);
 }
 

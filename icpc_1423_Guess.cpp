bf6d34c6c19f0c8118895e880b62d481d5444f34
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2021-10-16 17:59:46 +0300

    Enhanced the code and removed commented out prints

--- icpc_1423_Guess.cpp
+++ icpc_1423_Guess.cpp
@@ -5,11 +5,9 @@
 #include <numeric>
 using namespace std;
 
-
 int sum_subrow(vector<int>* guess, int from, int to){
 	return accumulate(guess->begin() + from, guess->begin() + to + 1, 0);
 }
-
 
 bool validate_sub_column(vector<int>* guess, vector<vector<char>>* mat, int guess_count, int col){
 	for (int i = 0; i < guess_count; i++){
@@ -26,37 +24,15 @@ bool validate_sub_column(vector<int>* guess, vector<vector<char>>* mat, int gues
 	return true;
 }
 
-bool validate_sub_matrix(vector<int>* guess, vector<vector<char>>* mat, int guess_count){
-	for (int i = 0; i < guess_count; i++){
-		for (int j = 0; j < guess_count; j++){
-			if (i <= j){
-//				printf("from %d to %d = %d \n", i, j, sum_subrow(guess, i, j));
-				if ((*mat)[i][j] == '+' && sum_subrow(guess, i, j) <= 0){
-					return false;
-				} else if ((*mat)[i][j] == '-' && sum_subrow(guess, i, j) >= 0){
-					return false;
-				} else if ((*mat)[i][j] == '0' && sum_subrow(guess, i, j) != 0){
-					return false;
-				}
-			}
-		}
-	}
-	return true;
-}
-
-
 bool recursive_solve(vector<int>* guess, int next, int limit, vector<vector<char>>* matrix_ptr){
-//	printf("-DD- recursive_solve: next = %d  \n", next);
 	if (next == limit){
-		return validate_sub_matrix(guess, matrix_ptr, limit);
-	} else if (!validate_sub_column(guess, matrix_ptr, next, next- 1)){
+		return validate_sub_column(guess, matrix_ptr, next, next - 1);
+	} else if (!validate_sub_column(guess, matrix_ptr, next, next - 1)){
 		return false;
 	}
 	else {
-//		printf("-DDD- recursive_solve: else. (*matrix_ptr)[next][next] = %d %d\n", (*matrix_ptr)[next][next], '-');
 		if ((*matrix_ptr)[next][next] == '0'){
 			(*guess)[next] = 0;
-//			printf("-D1- a[%d] = %d\n", next, 0);
 			if (recursive_solve(guess, next + 1, limit, matrix_ptr)){
 				return true;
 			}
@@ -64,7 +40,6 @@ bool recursive_solve(vector<int>* guess, int next, int limit, vector<vector<char
 		if ((*matrix_ptr)[next][next] == '+'){
 			for (int i = 1; i <= 10; i++){
 				(*guess)[next] = i;
-//				printf("-D2- a[%d] = %d\n", next, i);
 				if (recursive_solve(guess, next + 1, limit, matrix_ptr)){
 					return true;
 				}
@@ -73,17 +48,14 @@ bool recursive_solve(vector<int>* guess, int next, int limit, vector<vector<char
 		if ((*matrix_ptr)[next][next] == '-'){
 			for (int i = -1; i >= -10; i--){
 				(*guess)[next] = i;
-//				printf("-D3- a[%d] = %d\n", next, i);
 				if (recursive_solve(guess, next + 1, limit, matrix_ptr)){
 					return true;
 				}
 			}
 		}
 	}
-//	printf("-D- recursive_solve: END => false\n");
 	return false;
 }
-
 
 void solve(int n) {
 	vector<int> sol(n);
@@ -91,6 +63,8 @@ void solve(int n) {
 	for (int i = 0 ; i < n ; i++) {
 		matrix[i].resize(n);
 	}
+
+	// input:
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++){
 			if (i <= j){
@@ -100,22 +74,10 @@ void solve(int n) {
 	}
 	scanf("\n");
 
-	// DEBUG
-//	printf("===== SOLVE =====\n");
-//	for (int i = 0; i < n; i++){
-//		for (int j = 0; j < n; j++){
-//			if (i <= j){
-//				printf("mat[%d][%d] = %c  ", i, j, matrix[i][j]);
-//			}
-//		}
-//		printf("XX \n");
-//	}
-
-
+	// solve:
 	recursive_solve(&sol, 0, n, &matrix);
 
-
-
+	// print solution:
 	for (int i = 0 ; i < n - 1 ; i++) {
 		printf("%d ", sol[i]);
 	}
@@ -133,5 +95,3 @@ int main() {
 	}
 	return 0;
 }
-
-
